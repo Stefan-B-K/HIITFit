df4fd9d2bb678a94cba8a9f039f45ece8a7fe4a4
iStef <stefan_b_k@icloud.com>
2021-12-20 13:15:37 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Стефан Кожухаров on 20.12.21.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
